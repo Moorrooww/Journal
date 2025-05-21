a31aed86d07cbc043046d6b3eb67a4820bfd0330
Moorrooww <maurodossantos849@gmail.com>
2025-05-21 08:37:03 +0200

    new

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
+
+
 function getData() {
    fetch('data.json')
      .then((response) => {
@@ -11,6 +13,73 @@ function getData() {
        console.log('Données récupérées du fichier JSON :', data);
        /// ON ECRIT LE CODE ICI ! 
       
+      let conteneur = document.getElementById("navBar");
+      console.log(conteneur);
+      
+      let div1 = document.getElementById("div1")
+      console.log(div1);
+
+
+      
+      
+
+      let logo = document.createElement("img");
+      logo.src = "images/logo.jpg";
+      console.log(logo);
+
+      let JournalName = document.createElement("h1");
+      JournalName.textContent = data.journal.nomJournal;
+      
+      console.log(JournalName);
+      
+      let listenav = document.getElementById("listeNav");
+      console.log(listenav);
+
+      let liste = document.createElement("li");
+      console.log(liste);
+      
+      let acceuil = document.createElement ("a");
+      acceuil.textContent = "Acceuil"
+      acceuil.href = "#"
+      console.log(acceuil);
+
+      let liste2 = document.createElement("li");
+      console.log(liste2);
+
+      let news = document.createElement ("a");
+      news.textContent = "News"
+      news.href = "#"
+      console.log(news);
+      
+      
+      
+
+      
+      
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
       
 
@@ -22,3 +91,4 @@ function getData() {
  getData();
 
  ///ON écrit les fonctions ici
+
