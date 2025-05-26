0e10ef5a0a4eea02b922a15f1a6fcf8b4b8452a5
Moorrooww <maurodossantos849@gmail.com>
2025-05-26 09:42:39 +0200

    Push

--- script.js
+++ script.js
@@ -88,28 +88,195 @@ function getData() {
         // dire que les boutons sont les enfants d'un élément plus grand => id="button-container"
         let buttoncontain = document.getElementById("button-container");
 
-        buttoncontain.appendChild(bouton);
+          buttoncontain.appendChild(bouton);
+          console.log(buttoncontain);
+
+      //////////////////////////////////AJOUTER LE IF HOVER
+
+
 
 
       });
 
-
-
-
-
-
-
-
-     
-
-
-
-
-
-      let boutton = document.getElementsByClassName("themes");
-      boutton.textContent = data.journal.themes[0].nom;
-      console.log(boutton);
       
+
+
+///////////////////////
+
+      let articleConteneur = document.getElementById("cartes-conteneur");
+      console.log(articleConteneur);
+
+      let phraseAccro = document.createElement("h1");
+      phraseAccro.textContent = data.journal.phraseAccroche
+      console.log(phraseAccro);
+
+      let conteurPr = document.createElement("div");
+      conteurPr.id = "conteur-pr";
+      console.log(conteurPr);
+
+      let articlePrImage = document.createElement("img");
+      articlePrImage.src = data.journal.articlePrincipal.image
+      console.log(articlePrImage);
+      
+      let articlePrTheme = document.createElement("p");
+      articlePrTheme.textContent = data.journal.articlePrincipal.theme
+      console.log(articlePrTheme);
+      
+      let articlePrTitre = document.createElement("h2");
+      articlePrTitre.textContent = data.journal.articlePrincipal.titre
+      console.log(articlePrTitre);
+      
+      let articlePrDesc = document.createElement("p");
+      articlePrDesc.textContent = data.journal.articlePrincipal.description
+      console.log(articlePrDesc);
+      
+      let articlePrDate = document.createElement("h3");
+      articlePrDate.textContent = data.journal.articlePrincipal.date
+      console.log(articlePrDate);
+      
+
+      conteurPr.append(articlePrTheme,articlePrTitre,articlePrDesc,articlePrDate);
+
+      articleConteneur.append(articlePrImage,conteurPr)
+
+      let test = document.getElementById("test");
+
+      test.append(phraseAccro,articleConteneur);
+
+////////////////
+
+      let conteneurPart = document.getElementById("conteneur");
+      console.log(conteneurPart);
+
+
+      
+
+      let divPart = document.getElementById("cartes-conteneurs");
+      console.log(divPart);
+
+
+  
+
+      let conteneurP1 = document.createElement("div")
+      conteneurP1.className = "contentarticle";
+      console.log(conteneurP1);
+
+
+      function afficherArticles(article, divPart, conteneurPart) {
+          let conteneurP1 = document.createElement("div");
+          conteneurP1.className = "contentarticle";
+          console.log(conteneurP1);
+
+          let articlePtext = document.createElement("div");
+          articlePtext.className = "contentText";
+
+          let articlePimg = document.createElement("img");
+          articlePimg.src = article.image;
+          console.log(articlePimg);
+
+          let articlePtheme = document.createElement("p");
+          articlePtheme.textContent = article.theme;
+          console.log(articlePtheme);
+
+          let articlePtitre = document.createElement("h2");
+          articlePtitre.textContent = article.titre;
+          console.log(articlePtitre);
+
+          let articlePdate = document.createElement("h3");
+          articlePdate.textContent = article.date;
+          console.log(articlePdate);
+
+          articlePtext.append(articlePtheme, articlePtitre,articlePdate);
+
+          conteneurP1.append(articlePimg,articlePtext);
+
+          divPart.appendChild(conteneurP1);
+       
+
+        conteneurPart.appendChild(divPart);
+        console.log(conteneurPart);
+      }
+
+       data.journal.articles.forEach(article => {
+        afficherArticles(article, divPart, conteneurPart)
+       });
+
+//////////////////////
+
+       let containAuteur = document.getElementById("auteurcontain");
+       console.log(containAuteur);
+
+       let divAuteur = document.createElement("div")
+       divAuteur.className = "Auteurdiv";
+       console.log(divAuteur);
+
+
+
+
+////////////////
+      function afficherAuteurs(auteur,containAuteur) {
+
+      let divParAuteur = document.createElement("div");
+      divParAuteur.className = "DivParAuteur";
+
+      let divInfoAuteur = document.createElement("div");
+      divInfoAuteur.className = "InfoAuteur";
+
+      let auteurPp = document.createElement("img")
+      auteurPp.src = auteur.image;
+      console.log(auteurPp);
+      
+
+      let prenom = document.createElement("h4");
+      prenom.textContent = auteur.prenom;
+
+      let presentation = document.createElement("p")
+      presentation.textContent = auteur.presentation;
+
+      let experience = document.createElement("h5")
+      experience.textContent = auteur.typeExperience;
+      console.log(experience);
+      
+
+
+       divInfoAuteur.append(prenom,experience,presentation);
+
+       divParAuteur.append(auteurPp,divInfoAuteur)
+
+       containAuteur.appendChild(divParAuteur)
+       console.log(containAuteur);
+       }
+
+       data.journal.auteurs.forEach(auteur => {
+        afficherAuteurs(auteur,containAuteur)
+       });
+
+
+
+
+
+       
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
       
@@ -142,7 +309,7 @@ function getData() {
       
 
        /// FIN DU CODE
-     })
+ })
      .catch((error) => console.error('Erreur lors de la lecture des données :', error));
  }
  
@@ -153,8 +320,3 @@ function getData() {
 
 
 
-
-
-//   <header></header>
-///  <div id="divslog"></div>
-//// <div id="div2"></div>
